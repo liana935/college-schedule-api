7b4d84068597ca906cf1c1117b1aaf53499a9115
Podstrelov Slava <podstrel385@gmail.com>
2026-02-03 18:54:33 +0700

    Added ScheduleController

---
+++ CollegeSchedule/Controllers/ScheduleController.cs
@@ -0,0 +1,28 @@
+﻿using CollegeSchedule.Data;
+using CollegeSchedule.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeSchedule.Controllers
+{
+    [ApiController]
+    [Route("api/schedule")]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IScheduleService _service;
+
+        public ScheduleController(IScheduleService service, AppDbContext db)
+        {
+            _service = service;
+        }
+
+        //GET: api/schedule/group/{groupName}?start=2026-01-12&end=2026-01-17
+        [HttpGet("group/{groupName}")]
+        public async Task<IActionResult> GetSchedule(string groupName, DateTime start, DateTime end)
+        {
+            //Вызываем бизнес-логику из сервиса
+            var result = await _service.GetScheduleForGroup(groupName, start.Date, end.Date);
+            //Возвращаем результат со статусом 200 ОК
+            return Ok(result);
+        }
+    }
+}
